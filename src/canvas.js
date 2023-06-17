06c90ee2381280579818508367df64a23d991ed7
Marcel Goethals <marcel@wolkenmachine.nl>
2023-06-17 19:43:00 +0100

    erase

--- src/canvas.js
+++ src/canvas.js
@@ -15,6 +15,7 @@ ctx.scale(dpr, dpr)
 let strokes = []
 
 let mouse_is_down = false
+let erase_mode = false
 
 // MOUSE EVENTS
 window.addEventListener("mousedown", e=> {
@@ -38,9 +39,19 @@ window.addEventListener("touchstart", e=>{
     for(const touch of e.touches) {
         if(touch.touchType === 'stylus') {
             let pos = Vec(touch.clientX, touch.clientY)
-            pos.force = touch.force
-            strokes.push([pos])
-            mouse_is_down = true
+            if(!erase_mode) {
+                pos.force = touch.force
+                strokes.push([pos])
+                mouse_is_down = true
+            } else {
+                mouse_is_down = true
+                erase(pos)
+            }
+        } else {
+            let pos = Vec(touch.clientX, touch.clientY)
+            if(Vec.dist(pos, Vec(10,window.innerHeight/2 - 25)) < 50) {
+                erase_mode = ! erase_mode
+            }
         }
     }
 })
@@ -49,8 +60,12 @@ window.addEventListener("touchmove", e=>{
     for(const touch of e.touches) {
         if(mouse_is_down && touch.touchType === 'stylus') {
             let pos = Vec(touch.clientX, touch.clientY)
-            pos.force = touch.force
-            strokes[strokes.length-1].push(pos)
+            if(!erase_mode) { 
+                pos.force = touch.force
+                strokes[strokes.length-1].push(pos)
+            } else {
+                erase(pos)
+            }
         }
     }
 })
@@ -59,9 +74,26 @@ window.addEventListener("touchend", e=>{
     mouse_is_down = false
 })
 
+function erase(pos) {
+    for (let i = 0; i < strokes.length; i++) {
+        const stroke = strokes[i];
+        let did_match = false
+        for (const point of stroke) {
+            if(Vec.dist(pos, point) < 10) {
+                did_match = true
+                break;
+            }
+        }
+        if(did_match) {
+            strokes.splice(i, 1)
+        }
+        
+    }
+}
+
 function render(){
     ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
-    ctx.strokeStyle = "#FF0000"
+    ctx.strokeStyle = "#000000"
 
     for(const stroke of strokes) {
         ctx.beginPath()
@@ -71,6 +103,15 @@ function render(){
         }
         ctx.stroke()
     }
+
+    ctx.beginPath()
+    ctx.rect(10,window.innerHeight/2 - 25,50,50)
+    if(erase_mode) {
+        ctx.stroke()
+    } else {
+        ctx.fill()
+    }
+    
 
     requestAnimationFrame(render)
 }
